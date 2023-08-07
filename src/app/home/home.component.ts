6113caaf5f0ef238dc55d36481084322cf1ad565
Zac <yihongle@buffalo.edu>
2023-08-07 10:23:58 +0800

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <input type="text" placeholder="Filter by city" #filter />
+      <button
+        class="primary"
+        type="button"
+        (click)="filterResults(filter.value)"
+      >
+        Search
+      </button>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
+    <!-- Square brackets let angular know this is a property, not a string. -->
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  filteredLocationList: HousingLocation[] = [];
+  housingLocationList: HousingLocation[] = [];
+  constructor(private housingService: HousingService) {
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterOutlet],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="baseUrl + housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent implements OnInit {
+  @Input() housingLocation!: HousingLocation;
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  ngOnInit() {
+    this.logg();
+  }
+
+  logg() {
+    console.log(this.housingLocation);
+  }
+}
